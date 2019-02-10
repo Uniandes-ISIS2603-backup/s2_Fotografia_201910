2c5ea591df8455b107ef77bf4a80ce930f693a78
Dany Benavides <da.benavides@uniandes.edu.co>
2019-02-10 10:59:00 -0500

    rest vacio

---
+++ s2_fotografia-api/src/main/java/co/edu/uniandes/csw/fotografia/dtos/PhotoDetailDTO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.fotografia.dtos;
+
+import java.util.List;
+
+/**
+ *
+ * @author estudiante
+ */
+public class PhotoDetailDTO extends PhotoDTO {
+    
+    //private List<Calification> calification;
+
+    public PhotoDetailDTO(){
+        
+    }
+    /**
+     * @return the calification
+     */
+    //public List<Calification> getCalification() {
+    //    return calification;
+    //}
+
+    /**
+     * @param calification the calification to set
+     */
+    //public void setCalification(List<Calification> calification) {
+    //    this.calification = calification;
+    //}
+    
+    
+    
+}
