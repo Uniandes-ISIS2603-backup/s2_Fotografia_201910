0a04b2fbae7155d777a3032a74bc5d2ec3625bd2
s.acostav <s.acostav@uniandes.edu.co>
2019-02-11 12:13:48 -0500

    Logica fotografo

--- s2_fotografia-back/src/main/java/co/edu/uniandes/csw/fotografia/ejb/FotografoLogic.java
+++ s2_fotografia-back/src/main/java/co/edu/uniandes/csw/fotografia/ejb/FotografoLogic.java
@@ -6,32 +6,58 @@
 package co.edu.uniandes.csw.fotografia.ejb;
 
 import co.edu.uniandes.csw.fotografia.entities.FotografoEntity;
+import co.edu.uniandes.csw.fotografia.persistence.FotografoPersistence;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 /**
  *
  * @author s.acostav
  */
+
+@Stateless
 public class FotografoLogic {
     
+     private static final Logger LOGGER = Logger.getLogger(FotografoLogic.class.getName());
+
+    @Inject
+    private FotografoPersistence persistence;
+    
     public FotografoEntity createFotografo(FotografoEntity fotografo){
-        return new FotografoEntity();
+        LOGGER.log(Level.INFO, "Inicia proceso de creación del fotografo");
+        FotografoEntity newFotografoEntity = persistence.create(fotografo);
+        LOGGER.log(Level.INFO, "Termina proceso de creación del fotografo");
+        return newFotografoEntity;
     }
     
     public FotografoEntity getFotografo(long pId){
-        return new FotografoEntity();
+        LOGGER.log(Level.INFO, "Inicia proceso de consultar el fotografo con id = {0}", pId);
+        FotografoEntity fotografoEntity = persistence.find(pId);
+        if (fotografoEntity == null) {
+            LOGGER.log(Level.SEVERE, "El fotografo con el id = {0} no existe", pId);
+        }
+        LOGGER.log(Level.INFO, "Termina proceso de consultar el fotografo con id = {0}", pId);
+        return fotografoEntity;
     }
     
     public List<FotografoEntity> getFotografos(){
-        return null;
+       LOGGER.log(Level.INFO, "Inicia proceso de consultar todos los fotografos");
+        List<FotografoEntity> lista = persistence.findAll();
+        LOGGER.log(Level.INFO, "Termina proceso de consultar todos los fotografos");
+        return lista;
     }
     
      public FotografoEntity updateFotografo(Long fotografoId, FotografoEntity entity){
-         return new FotografoEntity();
+       LOGGER.log(Level.INFO, "Inicia proceso de actualizar el fotografo con id = {0}", fotografoId);
+        FotografoEntity newFotografoEntity = persistence.update(entity);
+        LOGGER.log(Level.INFO, "Termina proceso de actualizar el fotografo con id = {0}", fotografoId);
+        return newFotografoEntity;
      }
      
      public void deleteFotografo(Long fotografoId){
-         
      }
      
 }
