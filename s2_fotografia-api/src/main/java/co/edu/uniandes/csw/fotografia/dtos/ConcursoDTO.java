0e8eb8234ac2f0df89a2f6a4b94e960dd52eaacb
NicolasRinconD <n.rincond@uniandes.edu.co>
2019-03-04 01:44:31 -0500

    Complete unas funciones de los DTOS

--- s2_fotografia-api/src/main/java/co/edu/uniandes/csw/fotografia/dtos/ConcursoDTO.java
+++ s2_fotografia-api/src/main/java/co/edu/uniandes/csw/fotografia/dtos/ConcursoDTO.java
@@ -38,167 +38,215 @@ import org.apache.commons.lang3.builder.ToStringStyle;
  * </pre> 
  * @author n.rincond
  */
-public class ConcursoDTO implements Serializable  {
-	private Long id;
-	private String tema;
-	private List<String> restricciones ;
-	private Integer edadDeLaFoto;
-	private Integer maxFotos;
-	private Date fechaDelConcurso;
-	private Integer premioCantidad;
+public class ConcursoDTO implements Serializable {
 
-	//private OrganizadorDTO organizador
+    private Long id;
+    private String tema;
+    private List<String> restricciones;
+    private Integer edadDeLaFoto;
+    private Integer maxFotos;
+    private Date fechaDelConcurso;
+    private Integer premioCantidad;
 
+    private OrganizadorDTO organizador;
+    private RondaDTO ronda;
 
-	/**
-	 * Constructor vacio
-	 */
-	public ConcursoDTO(){
+    /**
+     * Constructor vacio
+     */
+    public ConcursoDTO() {
 
-	}
-        
-        public ConcursoDTO(ConcursoEntity concursoEntity){
-            if(concursoEntity != null){
-                this.id = concursoEntity.getId();
-                this.tema = concursoEntity.getTema();
-                this.restricciones = concursoEntity.getRestricciones();
-                this.edadDeLaFoto = concursoEntity.getEdadFoto();
-                this.maxFotos = concursoEntity.getMaximasFotos();
-                this.premioCantidad = concursoEntity.getCantidadPremio();
-                this.fechaDelConcurso = concursoEntity.getFecha();
+    }
+
+    public ConcursoDTO(ConcursoEntity concursoEntity) {
+        if (concursoEntity != null) {
+            this.id = concursoEntity.getId();
+            this.tema = concursoEntity.getTema();
+            this.restricciones = concursoEntity.getRestricciones();
+            this.edadDeLaFoto = concursoEntity.getEdadFoto();
+            this.maxFotos = concursoEntity.getMaximasFotos();
+            this.premioCantidad = concursoEntity.getCantidadPremio();
+            this.fechaDelConcurso = concursoEntity.getFecha();
+             if (concursoEntity.getOrganizador() != null) {
+                this.organizador = new OrganizadorDTO(concursoEntity.getOrganizador());
+            } else {
+                concursoEntity.setOrganizador(null);
             }
+             if(concursoEntity.getRonda() != null){
+                 this.ronda = new RondaDTO(concursoEntity.getRonda());
+             }
+             else{
+                 concursoEntity.setRonda(null);
+             }
         }
-        
-        public ConcursoEntity toEntity(){
-            ConcursoEntity entity = new ConcursoEntity();
-            entity.setId(this.getId());
-            entity.setCantidadPremio(this.premioCantidad);
-            entity.setFecha(this.fechaDelConcurso);
-            entity.setMaximasFotos(this.maxFotos);
-            entity.setEdadFoto(this.edadDeLaFoto);
-            entity.setTema(this.tema);
-            entity.setRestricciones(this.restricciones);
-            return entity;
-        }
-	/**
-	 *Obtiene el atributo id 
-	 * @return atributo id
-	 */
+    }
 
-	public Long getId(){
-		return id;
-	}
-	/**
-	 * Establece un valor para el atributoid
-	 * @param pId nuevo valor del atributo 
-	 */
-	public void setId(Long pId){
-		id = pId;
-	}
+    public ConcursoEntity toEntity() {
+        ConcursoEntity entity = new ConcursoEntity();
+        entity.setId(this.getId());
+        entity.setCantidadPremio(this.premioCantidad);
+        entity.setFecha(this.fechaDelConcurso);
+        entity.setMaximasFotos(this.maxFotos);
+        entity.setEdadFoto(this.edadDeLaFoto);
+        entity.setTema(this.tema);
+        entity.setRestricciones(this.restricciones);
+        if(organizador != null )entity.setOrganizador(organizador.toEntity());
+        if(ronda != null) entity.setRonda(ronda.toEntity());
+        return entity;
+    }
 
-	/**
-	 * Obtiene el atributo tema
-	 * @return atributo tema
-	 */
+    /**
+     * Obtiene el atributo id
+     *
+     * @return atributo id
+     */
 
-	public String getTema(){
-		return tema;
-	}
-	/**
-	 * Establece un valor para el atributo tema
-	 * @param pTema nuevo valor para el atributo 
-	 */
-	public void setTema(String pTema){
-		tema = pTema;
-	}
-	/**
-	 * Obtiene el atributo restricciones
-	 * @return atributo restricciones
-	 */
-	public List<String> getRestricciones(){
-		return restricciones;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	/**
-	 * Establece un valor para el atributo restricciones
-	 * @param pRestricciones nueva lista de restricciones
-	 */
-	public void setRestricciones(ArrayList<String> pRestricciones){
-		restricciones = pRestricciones;
-	}
-	/**
-	 * Agrega una restriccion a la lista de restricciones
-	 *
-	 */
-	public void addRestriccion(String pRestriccion)
-	{
-		restricciones.add(pRestriccion);
-	}
-	/**
-	 * Obtiene el atributo fechaConcurso
-	 * @return atributo fechaConcurso
-	 */
-	public Date getFechaConcurso(){
-		return fechaDelConcurso;
-	}
+    /**
+     * Establece un valor para el atributoid
+     *
+     * @param pId nuevo valor del atributo
+     */
+    public void setId(Long pId) {
+        id = pId;
+    }
 
-	/**
-	 * Establece un valor para el atributo fecha concurso
-	 * @param pFecha nuevo valor para el atributo 
-	 */
-	public void setFechaConcurso(Date pFechaConcurso){
-                fechaDelConcurso = pFechaConcurso;
-	}
+    /**
+     * Obtiene el atributo tema
+     *
+     * @return atributo tema
+     */
+    public String getTema() {
+        return tema;
+    }
 
+    /**
+     * Establece un valor para el atributo tema
+     *
+     * @param pTema nuevo valor para el atributo
+     */
+    public void setTema(String pTema) {
+        tema = pTema;
+    }
 
-	/**
-	 * Obtiene el atributo edad de la foto
-	 * @return atributo edad de la foto
-	 */
-	public int getEdadDeLaFoto(){
-		return edadDeLaFoto;
-	}
+    /**
+     * Obtiene el atributo restricciones
+     *
+     * @return atributo restricciones
+     */
+    public List<String> getRestricciones() {
+        return restricciones;
+    }
 
-	/**
-	 * Establece un valor para el atributo edad de la fotografia
-	 * @param pEdad nuevo valor para el atributo edadDeLaFoto
-	 */
-	public void setEdadDeLaFoto(int pEdad) {
-		edadDeLaFoto = pEdad;
-	}
+    /**
+     * Establece un valor para el atributo restricciones
+     *
+     * @param pRestricciones nueva lista de restricciones
+     */
+    public void setRestricciones(ArrayList<String> pRestricciones) {
+        restricciones = pRestricciones;
+    }
 
+    /**
+     * Agrega una restriccion a la lista de restricciones
+     *
+     */
+    public void addRestriccion(String pRestriccion) {
+        restricciones.add(pRestriccion);
+    }
 
-	/**
-	 *Obtiene el atributo maxFotos
-	 * @return atributo maxFotos
-	 */
-	public int getMaxFotos(){
-		return maxFotos;
-	}
-	/**
-	 * Establece un valor para el atributo maxFotos
-	 * @param max nuevo valor del atributo 
-	 */
+    /**
+     * Obtiene el atributo fechaConcurso
+     *
+     * @return atributo fechaConcurso
+     */
+    public Date getFechaConcurso() {
+        return fechaDelConcurso;
+    }
 
-	public void setMaxFotos(int max){
-		maxFotos = max;
-	}
-	/**
-	 *Obtiene el atributo premio
-	 * @return atributo premio
-	 */
+    /**
+     * Establece un valor para el atributo fecha concurso
+     *
+     * @param pFecha nuevo valor para el atributo
+     */
+    public void setFechaConcurso(Date pFechaConcurso) {
+        fechaDelConcurso = pFechaConcurso;
+    }
 
-	public int getPremioCantidad(){
-		return premioCantidad;
-	}
-	/**
-	 * Establece un valor para el atributo premio
-	 * @param pPremio nuevo valor del premio
-	 */
-	public void setPremioCantidad(int pPremio){
-		premioCantidad = pPremio;
-	}
+    /**
+     * Obtiene el atributo edad de la foto
+     *
+     * @return atributo edad de la foto
+     */
+    public int getEdadDeLaFoto() {
+        return edadDeLaFoto;
+    }
 
+    /**
+     * Establece un valor para el atributo edad de la fotografia
+     *
+     * @param pEdad nuevo valor para el atributo edadDeLaFoto
+     */
+    public void setEdadDeLaFoto(int pEdad) {
+        edadDeLaFoto = pEdad;
+    }
+
+    /**
+     * Obtiene el atributo maxFotos
+     *
+     * @return atributo maxFotos
+     */
+    public int getMaxFotos() {
+        return maxFotos;
+    }
+
+    /**
+     * Establece un valor para el atributo maxFotos
+     *
+     * @param max nuevo valor del atributo
+     */
+
+    public void setMaxFotos(int max) {
+        maxFotos = max;
+    }
+
+    /**
+     * Obtiene el atributo premio
+     *
+     * @return atributo premio
+     */
+
+    public int getPremioCantidad() {
+        return premioCantidad;
+    }
+
+    /**
+     * Establece un valor para el atributo premio
+     *
+     * @param pPremio nuevo valor del premio
+     */
+    public void setPremioCantidad(int pPremio) {
+        premioCantidad = pPremio;
+    }
+
+    public OrganizadorDTO getOrganizador() {
+        return organizador;
+    }
+
+    public void setOrganizador(OrganizadorDTO organizador) {
+        this.organizador = organizador;
+    }
+
+    public RondaDTO getRonda() {
+        return ronda;
+    }
+
+    public void setRonda(RondaDTO ronda) {
+        this.ronda = ronda;
+    }
 
 
 }
--- s2_fotografia-api/src/main/java/co/edu/uniandes/csw/fotografia/dtos/ConcursoDetailDTO.java
+++ s2_fotografia-api/src/main/java/co/edu/uniandes/csw/fotografia/dtos/ConcursoDetailDTO.java
@@ -1,55 +1,93 @@
 package co.edu.uniandes.csw.fotografia.dtos;
 
 import co.edu.uniandes.csw.fotografia.entities.ConcursoEntity;
+import co.edu.uniandes.csw.fotografia.entities.FotografoEntity;
+import co.edu.uniandes.csw.fotografia.entities.JuradoEntity;
+import co.edu.uniandes.csw.fotografia.entities.PhotoEntity;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConcursoDetailDTO extends ConcursoDTO implements Serializable {
-	
-	private List<JuradoDTO> jurados;
-	//private List<RondasDTO> rondas;
-        private List<PhotoDTO> fotos;
-        
-        private List<FotografoDTO> fotografos;
-        
-	public ConcursoDetailDTO() {
-            super();
-	}
-        
-        public ConcursoDetailDTO(ConcursoEntity concursoEntity){
-            super(concursoEntity);
-            if(concursoEntity != null){
-                //TODO
+
+    private List<JuradoDTO> jurados;
+
+    private List<PhotoDTO> fotos;
+
+    private List<FotografoDTO> fotografos;
+
+    public ConcursoDetailDTO() {
+        super();
+    }
+
+    public ConcursoDetailDTO(ConcursoEntity concursoEntity) {
+        super(concursoEntity);
+        if (concursoEntity != null) {
+            jurados = new ArrayList<>();
+            for (JuradoEntity entityJurados : concursoEntity.getJurados()) {
+                jurados.add(new JuradoDTO(entityJurados));
             }
+            fotos = new ArrayList<>();
+           for(PhotoEntity entityFotos : concursoEntity.getFotosEnConcurso()){
+               fotos.add(new PhotoDTO(entityFotos));
+           }
+           fotografos = new ArrayList<>();
+           for(FotografoEntity entityFotografos : concursoEntity.getFotografos()){
+               fotografos.add(new FotografoDTO(entityFotografos));
+           }
         }
-        
-        @Override
-        public ConcursoEntity toEntity(){
-            //TODO
-            return null;
+    }
+
+    @Override
+    public ConcursoEntity toEntity() {
+        ConcursoEntity concursoEntity = super.toEntity();
+        if(jurados != null){
+            List<JuradoEntity> juradosEntity  = new ArrayList<>();
+            for(JuradoDTO dtoJurado: jurados){
+                juradosEntity.add(dtoJurado.toEntity());
+            }
+            concursoEntity.setJurados(juradosEntity);
         }
-        
-        public List<JuradoDTO> getJurados(){
-            return jurados;
+        if(fotos != null){
+            List<PhotoEntity> juradosEntity  = new ArrayList<>();
+            for(PhotoDTO dtoPhoto: fotos){
+                juradosEntity.add(dtoPhoto.toEntity());
+            }
+            concursoEntity.setFotosEnConcurso(juradosEntity);
         }
-        
-        public void setJurados(List<JuradoDTO> pJurados){
-            jurados = pJurados;
+        if(fotografos != null){
+            List<FotografoEntity> juradosEntity  = new ArrayList<>();
+            for(FotografoDTO dtoFotografo: fotografos){
+                juradosEntity.add(dtoFotografo.toEntity());
+            }
+            concursoEntity.setFotografos(juradosEntity);
         }
-        
-        public List<PhotoDTO> getFotos(){
-            return fotos;
-        }
-        
-        public void setFotos(List<PhotoDTO> pFotos){
-            fotos = pFotos;
-        }
-        
-        public List<FotografoDTO> getFotografos(){
-            return fotografos;
-        }
-        
-        public void setFotogorafos(List<FotografoDTO> pFotografos){
-            fotografos = pFotografos;
-        }
+        return concursoEntity;
+    }
+
+    public List<JuradoDTO> getJurados() {
+        return jurados;
+    }
+
+    public void setJurados(List<JuradoDTO> pJurados) {
+        jurados = pJurados;
+    }
+
+    public List<PhotoDTO> getFotos() {
+        return fotos;
+    }
+
+    public void setFotos(List<PhotoDTO> pFotos) {
+        fotos = pFotos;
+    }
+
+    public List<FotografoDTO> getFotografos() {
+        return fotografos;
+    }
+
+    public void setFotogorafos(List<FotografoDTO> pFotografos) {
+        fotografos = pFotografos;
+    }
+
+
 }
