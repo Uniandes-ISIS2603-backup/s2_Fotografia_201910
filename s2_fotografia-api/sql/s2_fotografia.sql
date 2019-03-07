ef801d93f55d45744d19e37f8ed92cedf23bb2ad
nicolasmelendeznova <ng.melendez@uniandes.edu.co>
2019-03-07 10:31:25 -0500

    test postman

--- s2_fotografia-api/sql/s2_fotografia.sql
+++ s2_fotografia-api/sql/s2_fotografia.sql
@@ -1,7 +1,11 @@
 delete from CalificacionEntity;
 delete from ClienteEntity;
-delete from JURADOENTITY;
+delete from JuradoEntity;
+delete from OrganizadorEntity;
+delete from RondaEntity;
 
 insert into CalificacionEntity (id, puntaje, comentario) values (100, 4, 'Muy bonita');
 insert into ClienteEntity (id, usuario, nombre, correo) values (100, 'Andrea', 'Andrea Lopez','andreLopez@hotmail.com');
 insert into JuradoEntity(id, nombre, apellido, correo, cedula, pais, ciudad) values (400, 'Pedro', 'Ramirez', 'pedroramirez@yahoo.com', 12345678, 'Colombia', 'Bogota');
+insert into OrganizadorEntity(id, nombre, apellido, edad, correo, telefono, pais) values (200, 'Jose', 'Martinez', 'j.martinez@gmail.com', 31578965, 'Colombia');
+insert into RondaEntity(id, numeroRonda) values (200, 1);
