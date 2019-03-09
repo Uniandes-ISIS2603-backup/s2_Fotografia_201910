1c6d1e05b395a8c51f3d53da266096d6ae215480
atrujilloa <a.trujilloa1@uniandes.edu.co>
2019-03-08 20:45:17 -0500

    Fin

--- s2_fotografia-api/src/main/java/co/edu/uniandes/csw/fotografia/dtos/JuradoDetailDTO.java
+++ s2_fotografia-api/src/main/java/co/edu/uniandes/csw/fotografia/dtos/JuradoDetailDTO.java
@@ -5,8 +5,15 @@
  */
 package co.edu.uniandes.csw.fotografia.dtos;
 
+import co.edu.uniandes.csw.fotografia.entities.CalificacionEntity;
+import co.edu.uniandes.csw.fotografia.entities.ConcursoEntity;
+import co.edu.uniandes.csw.fotografia.entities.PhotoEntity;
+import co.edu.uniandes.csw.fotografia.entities.FormaDePagoEntity;
 import co.edu.uniandes.csw.fotografia.entities.JuradoEntity;
+import co.edu.uniandes.csw.fotografia.entities.RondaEntity;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -15,7 +22,10 @@ import org.apache.commons.lang3.builder.ToStringStyle;
  * @author a.trujilloa1
  */
 public class JuradoDetailDTO extends JuradoDTO implements Serializable {
-     
+
+    private List<PhotoDTO> fotos;
+    private List<CalificacionDTO> calificaciones;
+    
     public JuradoDetailDTO() {
         super();
     }
@@ -29,7 +39,17 @@ public class JuradoDetailDTO extends JuradoDTO implements Serializable {
      *
      */
     public JuradoDetailDTO(JuradoEntity juradoEntity) {
-        
+        super(juradoEntity);
+        if (juradoEntity != null) {
+            fotos = new ArrayList<>();
+            for (PhotoEntity photoEntity : juradoEntity.getFotos()) {
+                fotos.add(new PhotoDTO(photoEntity));
+            }
+            calificaciones = new ArrayList();
+            for ( CalificacionEntity entityCalificacion : juradoEntity.getCalificaciones()) {
+                calificaciones.add(new CalificacionDTO(entityCalificacion));
+            }
+        }
     }
 
     /**
