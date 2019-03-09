0743f369d779a5e3575783cc8f31bd5cfd9e50ba
atrujilloa <a.trujilloa1@uniandes.edu.co>
2019-03-08 20:10:49 -0500

    finalizado

---
+++ s2_fotografia-api/src/main/java/co/edu/uniandes/csw/fotografia/resources/CalificacionClienteResource.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.fotografia.resources;
+
+import co.edu.uniandes.csw.fotografia.dtos.CalificacionDetailDTO;
+import co.edu.uniandes.csw.fotografia.dtos.ClienteDTO;
+import co.edu.uniandes.csw.fotografia.ejb.CalificacionClienteLogic;
+import co.edu.uniandes.csw.fotografia.ejb.CalificacionLogic;
+import co.edu.uniandes.csw.fotografia.ejb.ClienteLogic;
+import co.edu.uniandes.csw.fotografia.ejb.ClienteLogic;
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author a.trujilloa1
+ */
+@Path("calificaciones/{calificacionesId: \\d+}/cliente")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class CalificacionClienteResource {
+     private static final Logger LOGGER = Logger.getLogger(CalificacionClienteResource.class.getName());
+
+    @Inject
+    private CalificacionLogic calificacionLogic; // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias.
+
+    @Inject
+    private CalificacionClienteLogic calificacionClienteLogic; // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias.
+
+    @Inject
+    private ClienteLogic clienteLogic; // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias.
+
+    /**
+     * Remplaza la instancia de Cliente asociada a un Book.
+     *
+     * @param calificacionesId Identificador del libro que se esta actualizando. Este
+     * debe ser una cadena de dígitos.
+     * @param calificacion La calificacion que se será del libro.
+     * @return JSON {@link CalificacionDetailDTO} - El arreglo de libros guardado en la
+     * calificacion.
+     * @throws WebApplicationException {@link WebApplicationExceptionMapper} -
+     * Error de lógica que se genera cuando no se encuentra la calificacion o el
+     * libro.
+     */
+    @PUT
+    public CalificacionDetailDTO replaceCliente(@PathParam("calificacionesId") Long calificacionesId, ClienteDTO calificacion) {
+        LOGGER.log(Level.INFO, "BookClienteResource replaceCliente: input: calificacionesId{0} , Cliente:{1}", new Object[]{calificacionesId, calificacion});
+        if (calificacionLogic.getCalificacion(calificacionesId) == null) {
+            throw new WebApplicationException("El recurso /calificacion/" + calificacionesId + " no existe.", 404);
+        }
+        if (clienteLogic.getCliente(calificacion.getId()) == null) {
+            throw new WebApplicationException("El recurso /calificacions/" + calificacion.getId() + " no existe.", 404);
+        }
+        CalificacionDetailDTO calificacionDetailDTO = new CalificacionDetailDTO(calificacionClienteLogic.replaceCliente(calificacionesId, calificacion.getId()));
+        LOGGER.log(Level.INFO, "BookClienteResource replaceCliente: output: {0}", calificacionDetailDTO);
+        return calificacionDetailDTO;
+    }
+    
+}
--- s2_fotografia-back/src/main/java/co/edu/uniandes/csw/fotografia/ejb/CalificacionPhotoLogic.java
+++ s2_fotografia-back/src/main/java/co/edu/uniandes/csw/fotografia/ejb/CalificacionPhotoLogic.java
@@ -6,10 +6,10 @@
 package co.edu.uniandes.csw.fotografia.ejb;
 
 import co.edu.uniandes.csw.fotografia.entities.CalificacionEntity;
-import co.edu.uniandes.csw.fotografia.entities.JuradoEntity;
+import co.edu.uniandes.csw.fotografia.entities.PhotoEntity;
 import co.edu.uniandes.csw.fotografia.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.fotografia.persistence.CalificacionPersistence;
-import co.edu.uniandes.csw.fotografia.persistence.JuradoPersistence;
+import co.edu.uniandes.csw.fotografia.persistence.PhotoPersistance;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
@@ -20,11 +20,11 @@ import javax.inject.Inject;
  * @author a.trujilloa1
  */
 @Stateless
-public class CalificacionJuradosLogic {
-    private static final Logger LOGGER = Logger.getLogger(CalificacionJuradosLogic.class.getName());
+public class CalificacionPhotoLogic {
+    private static final Logger LOGGER = Logger.getLogger(CalificacionPhotoLogic.class.getName());
 
     @Inject
-    private JuradoPersistence juradoPersistence; // Variable para acceder a la persistencia de la aplicación. Es una inyección de dependencias.
+    private PhotoPersistance photoPersistence; // Variable para acceder a la persistencia de la aplicación. Es una inyección de dependencias.
 
     @Inject
     private CalificacionPersistence callificacionPersistence; // Variable para acceder a la persistencia de la aplicación. Es una inyección de dependencias.
@@ -33,16 +33,16 @@ public class CalificacionJuradosLogic {
      * Agregar un jurado a un calficacion
      *
      * @param calificacionesId El id calficacion a guardar
-     * @param juradosId El id del jurado al cual se le va a guardar el calficacion.
+     * @param photoId El id del jurado al cual se le va a guardar el calficacion.
      * @return El calficacion que fue agregado al jurado.
      */
-    public JuradoEntity addJurado(Long juradosId, Long calificacionesId) {
-        LOGGER.log(Level.INFO, "Inicia proceso de asociar el jurado con id = {0} al calficacion con id = " + calificacionesId, juradosId);
-        JuradoEntity juradoEntity = juradoPersistence.find(juradosId);
+    public PhotoEntity addFoto(Long photoId, Long calificacionesId) {
+        LOGGER.log(Level.INFO, "Inicia proceso de asociar el jurado con id = {0} al calficacion con id = " + calificacionesId, photoId);
+        PhotoEntity photoEntity = photoPersistence.find(photoId);
         CalificacionEntity calificacionEntity = callificacionPersistence.find(calificacionesId);
-        calificacionEntity.setJuradoCalificador(juradoEntity);
-        LOGGER.log(Level.INFO, "Termina proceso de asociar el jurado con id = {0} al calficacion con id = " + calificacionesId, juradosId);
-        return juradoPersistence.find(juradosId);
+        calificacionEntity.setFotoCalificada(photoEntity);
+        LOGGER.log(Level.INFO, "Termina proceso de asociar el jurado con id = {0} al calficacion con id = " + calificacionesId, photoId);
+        return photoPersistence.find(photoId);
     }
 
     /**
@@ -52,26 +52,26 @@ public class CalificacionJuradosLogic {
      * @param calificacionesId id del calficacion a ser buscado.
      * @return el jurado solicitada por medio de su id.
      */
-    public JuradoEntity getJurado(Long calificacionesId) {
+    public PhotoEntity getFoto(Long calificacionesId) {
         LOGGER.log(Level.INFO, "Inicia proceso de consultar el jurado del calficacion con id = {0}", calificacionesId);
-        JuradoEntity juradoEntity = callificacionPersistence.find(calificacionesId).getJuradoCalificador();
+        PhotoEntity photoEntity = callificacionPersistence.find(calificacionesId).getFotoCalificada();
         LOGGER.log(Level.INFO, "Termina proceso de consultar el jurado del calficacion con id = {0}", calificacionesId);
-        return juradoEntity;
+        return photoEntity;
     }
 
     /**
      * Remplazar jurado de un calficacion
      *
      * @param calificacionesId el id del calficacion que se quiere actualizar.
-     * @param juradosId El id del nuebo jurado asociado al calficacion.
+     * @param photoId El id del nuebo jurado asociado al calficacion.
      * @return el nuevo jurado asociado.
      */
-    public CalificacionEntity replaceJurado(Long calificacionesId, Long juradosId) {
+    public CalificacionEntity replaceFoto(Long calificacionesId, Long photoId) {
         LOGGER.log(Level.INFO, "Inicia proceso de actualizar el jurado del calficacion calficacion con id = {0}", calificacionesId);
-        JuradoEntity juradoEntity = juradoPersistence.find(juradosId);
+        PhotoEntity photoEntity = photoPersistence.find(photoId);
         CalificacionEntity calificacionEntity = callificacionPersistence.find(calificacionesId);
-        calificacionEntity.setJuradoCalificador(juradoEntity);
-        LOGGER.log(Level.INFO, "Termina proceso de asociar el jurado con id = {0} al calficacion con id = " + calificacionesId, juradosId);
+        calificacionEntity.setFotoCalificada(photoEntity);
+        LOGGER.log(Level.INFO, "Termina proceso de asociar el jurado con id = {0} al calficacion con id = " + calificacionesId, photoId);
         return calificacionEntity;
     }
 
@@ -81,15 +81,15 @@ public class CalificacionJuradosLogic {
      * @param calificacionesId El calficacion que se desea borrar del jurado.
      * @throws BusinessLogicException si el calficacion no tiene jurado
      */
-    public void removeJurado(Long calificacionesId) throws BusinessLogicException {
+    public void removeFoto(Long calificacionesId) throws BusinessLogicException {
         LOGGER.log(Level.INFO, "Inicia proceso de borrar el jurado del calficacion con id = {0}", calificacionesId);
         CalificacionEntity calificacionEntity = callificacionPersistence.find(calificacionesId);
-        if (calificacionEntity.getJuradoCalificador()== null) {
+        if (calificacionEntity.getFotoCalificada()== null) {
             throw new BusinessLogicException("El calficacion no tiene jurado");
         }
-        JuradoEntity juradoEntity = juradoPersistence.find(calificacionEntity.getJuradoCalificador().getId());
-        calificacionEntity.setJuradoCalificador(null);
-        juradoEntity.getCalificaciones().remove(calificacionEntity);
-        LOGGER.log(Level.INFO, "Termina proceso de borrar el jurado con id = {0} del calficacion con id = " + calificacionesId, juradoEntity.getId());
+        PhotoEntity photoEntity = photoPersistence.find(calificacionEntity.getFotoCalificada().getId());
+        calificacionEntity.setFotoCalificada(null);
+        //photoEntity.getCalificaciones().remove(calificacionEntity);
+        LOGGER.log(Level.INFO, "Termina proceso de borrar el jurado con id = {0} del calficacion con id = " + calificacionesId, photoEntity.getId());
     }
 }
