753ccea6e84661c8177428a0d7ce7a9d908f0577
atrujilloa <a.trujilloa1@uniandes.edu.co>
2019-03-14 09:00:13 -0500

    Corregido

--- s2_fotografia-api/src/main/java/co/edu/uniandes/csw/fotografia/dtos/JuradoDetailDTO.java
+++ s2_fotografia-api/src/main/java/co/edu/uniandes/csw/fotografia/dtos/JuradoDetailDTO.java
@@ -6,11 +6,8 @@
 package co.edu.uniandes.csw.fotografia.dtos;
 
 import co.edu.uniandes.csw.fotografia.entities.CalificacionEntity;
-import co.edu.uniandes.csw.fotografia.entities.ConcursoEntity;
 import co.edu.uniandes.csw.fotografia.entities.PhotoEntity;
-import co.edu.uniandes.csw.fotografia.entities.FormaDePagoEntity;
 import co.edu.uniandes.csw.fotografia.entities.JuradoEntity;
-import co.edu.uniandes.csw.fotografia.entities.RondaEntity;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -40,11 +37,14 @@ public class JuradoDetailDTO extends JuradoDTO implements Serializable {
      */
     public JuradoDetailDTO(JuradoEntity juradoEntity) {
         super(juradoEntity);
-        if (juradoEntity != null) {
+        if (juradoEntity.getFotos() != null) {
             fotos = new ArrayList<>();
             for (PhotoEntity photoEntity : juradoEntity.getFotos()) {
                 fotos.add(new PhotoDTO(photoEntity));
             }
+        }
+        
+        if(juradoEntity.getCalificaciones() !=null){
             calificaciones = new ArrayList();
             for ( CalificacionEntity entityCalificacion : juradoEntity.getCalificaciones()) {
                 calificaciones.add(new CalificacionDTO(entityCalificacion));
@@ -62,9 +62,59 @@ public class JuradoDetailDTO extends JuradoDTO implements Serializable {
     @Override
     public JuradoEntity toEntity() {
         JuradoEntity juradoEntity = super.toEntity();
+        if( fotos != null){
+            List<PhotoEntity> fotosEntity = new ArrayList<>();
+            for( PhotoDTO dtoFoto : getFotos()){
+                fotosEntity.add(dtoFoto.toEntity());
+            }
+            juradoEntity.setFotos(fotosEntity);
+        }
+        if(calificaciones != null){
+            List<CalificacionEntity> calificacionesEntity = new ArrayList<>();
+            for( CalificacionDTO dtoCalificacion: getCalificaciones()){
+                calificacionesEntity.add(dtoCalificacion.toEntity());
+            }
+            juradoEntity.setCalificaciones(calificacionesEntity);
+        }
         return juradoEntity;
     }
 
+    /**
+     * Devuelve las reseñas asociadas a este jurado
+     *
+     * @return Lista de DTOs de Fotos
+     */
+    public List<PhotoDTO> getFotos() {
+        return fotos;
+    }
+
+    /**
+     * Modifica las fotos del jurado.
+     *
+     * @param fotos Las nuevas reseñas
+     */
+    public void setFotos(List<PhotoDTO> fotos) {
+        this.fotos = fotos;
+    }
+
+    /**
+     * Devuelve las calificaciones del jurado
+     *
+     * @return DTO de Calificaciones
+     */
+    public List<CalificacionDTO> getCalificaciones() {
+        return calificaciones;
+    }
+
+    /**
+     * Modifica los calificaciones del jurado
+     *
+     * @param calificaciones Lista de calificaciones
+     */
+    public void setCalificaciones(List<CalificacionDTO> calificaciones) {
+        this.calificaciones = calificaciones;
+    }
+    
     @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
