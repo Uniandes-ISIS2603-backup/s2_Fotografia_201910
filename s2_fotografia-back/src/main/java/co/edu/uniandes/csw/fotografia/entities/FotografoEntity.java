ce781bd2f129eba32eb257fdb184f95f789192ac
sacostav <42441785+sacostav@users.noreply.github.com>
2019-05-23 13:28:33 -0500

    Update FotografoEntity.java

--- s2_fotografia-back/src/main/java/co/edu/uniandes/csw/fotografia/entities/FotografoEntity.java
+++ s2_fotografia-back/src/main/java/co/edu/uniandes/csw/fotografia/entities/FotografoEntity.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import uk.co.jemos.podam.common.PodamExclude;
 
